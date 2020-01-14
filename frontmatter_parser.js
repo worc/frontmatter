a13de8e835c66c4eecae9742bab7cafa97883925
Curtis McAllister <cmcallister@workwithopal.com>
2020-01-13 22:52:53 -0800

    update frontmatter_parser to handle frontmatter line types

--- frontmatter_parser.js
+++ frontmatter_parser.js
@@ -1,21 +1,50 @@
-import validateFrontmatterLine from './validation'
+import validateFrontmatterLine, { BEGINNING_OF_LIST, KEY_VALUE_PAIR, LIST_ITEM } from './validation'
 
 /**
  *
- * @param {string} frontmatter
+ * @param {string} frontmatterString
+ * @returns {{}}
  */
-export default function parseFrontmatter ({ frontmatter }) {
-  const frontMatterBlankLinesRemoved = frontmatter
+export default function parseFrontmatter ({ frontmatter: frontmatterString }) {
+  const parsedFrontmatter = {}
+  const frontMatterBlankLinesRemoved = frontmatterString
     .split(/\n/g)
     .map(line => line.trim())
     .filter(line => line.length > 0)
 
-  frontMatterBlankLinesRemoved.every((line, index, array) => {
-    return validateFrontmatterLine({
+  const parsedFrontmatterList = frontMatterBlankLinesRemoved.map((line, index, array) => {
+    const type = validateFrontmatterLine({
       line,
       index,
       array,
       nextLine: array[index + 1],
     })
+
+    return {
+      type,
+      line,
+    }
   })
+
+  let currentList = ''
+  parsedFrontmatterList.forEach(parsedFrontmatterLine => {
+    if (parsedFrontmatterLine.type === KEY_VALUE_PAIR) {
+      const key = parsedFrontmatterLine.line.split(':', 2)[0].trim()
+      const value = parsedFrontmatterLine.line.split(':', 2)[1].trim()
+
+      parsedFrontmatter[key] = value
+    }
+
+    if (parsedFrontmatterLine.type === BEGINNING_OF_LIST) {
+      currentList = parsedFrontmatterLine.line.split(':', 2)[0].trim()
+      parsedFrontmatter[currentList] = []
+    }
+
+    if (parsedFrontmatterLine.type === LIST_ITEM) {
+      const value = parsedFrontmatterLine.line.split('-', 2)[1].trim()
+      parsedFrontmatter[currentList].push(value)
+    }
+  })
+
+  return parsedFrontmatter
 }
